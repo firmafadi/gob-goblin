e7fde0b1d419c89c362ba40459023ae238822aee
firmafadi <firmafirmansyah@gmail.com>
2020-01-06 16:58:47 +0700

    cek

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Goblin
                 </div>
 
                 <div class="links">
